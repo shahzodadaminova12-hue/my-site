07f5f0315d28665cc7ed514dec54c04bf016f13d
DD <49028092+sanjar3287889@users.noreply.github.com>
2025-08-14 14:28:12 +0500

    Initial commit: полный проект сайта

---
+++ js/script.js
@@ -0,0 +1,6 @@
+﻿document.addEventListener('DOMContentLoaded', function() {
+    const btn = document.getElementById('downloadBtn');
+    btn.addEventListener('click', function() {
+        alert('Файл начнет загружаться!');
+    });
+});
